27ebc71c90c22cec348d2e90204a7399e0b2647e
QingjieShen <122641381+QingjieShen@users.noreply.github.com>
2024-11-20 19:53:15 -0500

    adds data log

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ $anotherDayConfirm.addEventListener('click', async function(e) {
    $addCollectionBtn.innerHTML = `<i class="fa-regular fa-square-plus"></i>`
    const res = await fetch(url)
    const data = await res.json()
+   console.log(data)
    if (data.media_type === "image") {
       $showImg.innerHTML = ""
       const displayImage = document.createElement("img")
@@ -232,3 +233,24 @@ $goCollectionsPage.addEventListener('click', setCollectionsPage)
 $resultCollectionsPage.addEventListener('click', setCollectionsPage)
 $collectionGoBack.addEventListener('click', setChoosePage)
 
+
+/* {
+   "date": "2024-11-20",
+   "explanation": "ght billion people are about to disappear in this snapshot from space taken on 2022 November 21. On the sixth day of the Artemis I mission, their home world is setting behind the Moon's bright edge as viewed by an external camera on the outbound Orion spacecraft. Orion was headed for a powered flyby that took it to within 130 kilometers of the lunar surface. Velocity gained in the flyby maneuver was used to reach a distant retrograde orbit around the Moon. That orbit is considered distant because it's another 92,000 kilometers beyond the Moon, and retrograde because the spacecraft orbited in the opposite direction of the Moon's orbit around planet Earth. Orion entered its distant retrograde orbit on November 25. Swinging around the Moon, Orion reached a maximum distance (just over 400,000 kilometers) from Earth on November 28, exceeding a record set by Apollo 13 for most distant spacecraft designed for human space exploration. The Artemis II mission, carrying 4 astronauts around the moon and back again, is scheduled to launch no earlier than September 2025.",
+   "hdurl": "https://apod.nasa.gov/apod/image/2411/earthset-snap00.png",
+   "media_type": "image",
+   "service_version": "v1",
+   "title": "Earthset from Orion",
+   "url": "https://apod.nasa.gov/apod/image/2411/earthset-snap01.png"
+} */
+
+/*    {
+      "copyright": "\nJosh Dury\n",
+      "date": "2024-11-05",
+      "explanation": "Why were the statues on Easter Island built?  No one is sure.  What is sure is that over 900 large stone statues called moais exist there.  The Rapa Nui (Easter Island) moais stand, on average, over twice as tall as a person and have over 200 times as much mass.  It is thought that the unusual statues were created about 600 years ago in the images of local leaders of a vibrant and ancient civilization.  Rapa Nui has been declared by UNESCO to a World Heritage Site. Pictured here, some of the stone giants were imaged last month under the central band of our Milky Way galaxy.  Previously unknown moais are still being discovered.    Alternative Multi-APOD Front Page: MyUniverseHub.com",
+      "hdurl": "https://apod.nasa.gov/apod/image/2411/IslandMoai_Dury_2831.jpg",
+      "media_type": "image",
+      "service_version": "v1",
+      "title": "Milky Way over Easter Island",
+      "url": "https://apod.nasa.gov/apod/image/2411/IslandMoai_Dury_960.jpg"
+  } */
